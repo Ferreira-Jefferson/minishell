a73173ddea16439b722dc0bffce7d8517525f737
joaolive <joaolive@student.42sp.org.br>
2025-10-16 16:42:24 -0300

    ✨ chore: add libft

---
+++ include/minishell.h
@@ -0,0 +1,14 @@
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdbool.h>
+# include "lib/libft/include/libft.h"
+
+#endif
