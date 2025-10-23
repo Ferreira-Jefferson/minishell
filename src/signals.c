812e52917e448808cbbe12d8fab46572d5a55e03
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c2r12p3.42sp.org.br>
2025-10-23 15:31:31 -0300

    ♻️ refator: simplify signal manager

--- src/signals.c
+++ src/signals.c
@@ -19,22 +19,8 @@ void	handler(int signum)
 
 int	setup_signals(void)
 {
-	struct sigaction	sa_int;
-	struct sigaction	sa_quit;
-	struct sigaction	sa_stp;
-
-	g_signal_status = 0;
-	sigemptyset(&sa_int.sa_mask);
-	sa_int.sa_flags = 0;
-	sa_int.sa_handler = &handler;
-	sigaction(SIGINT, &sa_int, NULL);
-	sigemptyset(&sa_quit.sa_mask);
-	sa_quit.sa_flags = 0;
-	sa_quit.sa_handler = SIG_IGN;
-	sigaction(SIGQUIT, &sa_quit, NULL);
-	sigemptyset(&sa_stp.sa_mask);
-	sa_stp.sa_flags = 0;
-	sa_stp.sa_handler = SIG_IGN;
-	sigaction(SIGTSTP, &sa_stp, NULL);
+	signal(SIGINT, handler);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
 	return (0);
 }
